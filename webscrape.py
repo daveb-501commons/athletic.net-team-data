9d2594614ea6ee715ce983d2cfd691035a0fdb37
Dave Boyce <dboyceman@hotmail.com>
2021-01-24 23:16:05 -0800

    generate csv files

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,8 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+from decimal import *
+from random import randint
 
 url = "https://www.athletic.net/CrossCountry/Results/Meet.aspx?Meet=119954&show=all"
 r = requests.get(url)
@@ -11,49 +13,97 @@ if soup.title is not None:
 	meetName = str(soup.title.string)
 	meetName = meetName.replace('\n','').replace('\t','').replace('\r','').replace('/','_')
 
-	file_name = "meet-" + str(meetName) + (".txt")
-	text_file = open(file_name, "w")
-	text_file.write("Meet Name: %s\n" % meetName)
+	meetDetails = soup.find_all('div', {"class" : "meetDetails"})
+	eventDate = meetDetails[0].next.text
+	eventSeason = 'Winter'
+	if 'Dec' in eventDate or 'Jan' in eventDate or 'Feb' in eventDate:
+		eventSeason = 'Winter'
+	elif 'Mar' in eventDate or 'Apr' in eventDate or 'May' in eventDate:
+		eventSeason = 'Spring'
+	elif 'Jun' in eventDate or 'Jul' in eventDate or 'Aug' in eventDate:
+		eventSeason = 'Summer'
+	else:
+		eventSeason = 'Fall'
 
-	for resultSection in soup.find_all('div', {"class" : "tab-pane"}):
+	eventLocation = meetDetails[0].next.next.next.next.next.text
 
-		for event in resultSection.find_all('h3', {"class" : "mTop5"}):
-			eventClassName = str(event.text)
-			eventClassName = eventClassName.replace('\n','').replace('\t','').replace('\r','').replace(' ','')
-			text_file.write("\nEvent Class Name: %s\n" % eventClassName)
+	with open('races.csv', 'wb') as csvfile:
+		fieldnames_races = ['id', 'date', 'location', 'season', 'bg', 'race_name', 'distance', 'type', 'score', 'place', 'teams']
+		writer_races = csv.DictWriter(csvfile, fieldnames=fieldnames_races)
+		writer_races.writeheader()
 
-			for eventSummary in resultSection.find_all('table', {"class" : "DataTable HLData table table-hover"}):
+		with open('performances.csv', 'wb') as csvfile:
+			fieldnames_performances = ['id', 'ram', 'race', 'race_time', 'depth', 'place', 'grade', 'season', 'meet_team_rank']
+			writer_performances = csv.DictWriter(csvfile, fieldnames=fieldnames_performances)
+			writer_performances.writeheader()
 
-				for eventSubSummary in resultSection.find_all('tbody', {"class" : "DivBody"}):
+			for resultSection in soup.find_all('div', {"class" : "tab-pane"}):
 
-					# Team Event Type
-					for teamEventTypeSecion in eventSubSummary.find_all('tr', {"class" : "DivHeader"}):
-						for eventType in teamEventTypeSecion.find_all('h4', {"class" : ""}):
-							text_file.write("\nTeam Type Name: %s\n" % eventType.text)
+				for event in resultSection.find_all('h3', {"class" : "mTop5"}):
 
-					numberResults = 0
+					# Event Class Name - MensResults or WomensResults
+					eventClassName = str(event.text)
+					eventGender = 'b'
+					if "Women" in eventClassName:
+						eventGender = 'g'
 
-					# Team Scores
-					for teamScoreSecion in eventSubSummary.find_all('tr', {"class" : "ts_Cont"}):
+					eventType = 'unknown'
+					eventPlace = 'unknown'
+					eventScore = 'unknown'
+					eventDistanceMiles = 0
 
-						for eventResult in teamScoreSecion.find_all('tr', {"class" : "ts"}):
-							numberResults += 1
-							if "Ingraham" in eventResult.text:
-								text_file.write("Team Result: %s\n" % eventResult.text)
+					individualRamGenderDepth = 1
 
-						text_file.write("Total Team Results: %s\n\n" % numberResults)
+					for eventSummary in resultSection.find_all('table', {"class" : "DataTable HLData table table-hover"}):
 
-					# Individual Scores
-					for individualResult in eventSubSummary.find_all('tr', {"class" : ""}):
-						if not "Official Team Scores" in individualResult.text and "Ingraham" in individualResult.text:
-							text_file.write("Individual Result: %s\n" % individualResult.text)
+						for eventSubSummary in resultSection.find_all('tbody', {"class" : "DivBody"}):
 
+							# Team Event Type
+							eventFullType = 'unknown'
+							for teamEventTypeSecion in eventSubSummary.find_all('tr', {"class" : "DivHeader"}):
+								for eventFullTypeRecord in teamEventTypeSecion.find_all('h4', {"class" : ""}):
+									eventFullType = eventFullTypeRecord.text
+									if "Junior Varsity" in eventFullType:
+										eventType = 'jv'
+									elif "Varsity" in eventFullType:
+										eventType = 'v'
+									else:
+										eventType = 'mix'
 
-#		for event in soup.find_all('table', {"class" : "DataTable HLData table table-hover"}):
-#			for event_name in event.find_all('h5', {"class" : "bold"}):
-#				text_file.write("Event: %s " % str(event_name.contents[0]))
-#			for event_time in event.find_all('a', {"class" : " PR"}):
-#				text_file.write("Time: %s\n" % str(event_time.contents[0]))
+									eventDistanceMiles = Decimal(eventFullType.split(" ")[0].replace("," , "")) / Decimal(1600)
+
+							numberResults = 0
+
+							# Team Scores
+							for teamScoreSecion in eventSubSummary.find_all('tr', {"class" : "ts_Cont"}):
+
+								for eventResult in teamScoreSecion.find_all('tr', {"class" : "ts"}):
+									numberResults += 1
+									if "Ingraham" in eventResult.text:
+										results = eventResult.text.split(".Ingraham")
+										eventPlace = results[0]
+										eventScore = results[1]
+
+							race_id = randint(10000000, 99999999)
+
+							# Write Race Data
+							writer_races.writerow({'id' : race_id, 'date' : eventDate, 'location' : eventLocation, 'season': eventSeason, 'bg' : eventGender, 'race_name' : eventFullType, 'distance' : str(eventDistanceMiles), 'type' : eventType, 'score' : eventScore, 'place' : eventPlace, 'teams' : numberResults})	
+
+							# Individual Scores
+							individualRamDepth = 1
+							for individualResult in eventSubSummary.find_all('tr', {"class" : ""}):
+								if not "Official Team Scores" in individualResult.text and "Ingraham" in individualResult.text:
+
+									# Parse Text Value: 1.11Ian Stiehl19:05.83Ingraham which is Place.GradeNameTimeSchool
+									individualPlace = individualResult.next.text.replace(".", "")
+									individualGrade = individualResult.next.next.next.text
+									individualName = individualResult.next.next.next.next.next.text
+									individualTime = individualResult.next.next.next.next.next.next.next.text
+									
+									individual_id = randint(10000000, 99999999)
+
+									writer_performances.writerow({'id' : individual_id, 'ram' : individualName, 'race' : eventFullType, 'race_time' : individualTime, 'depth' : individualRamDepth, 'place' : individualPlace, 'grade' : individualGrade, 'season' : eventSeason, 'meet_team_rank' : individualRamGenderDepth})
+									individualRamDepth += 1
+									individualRamGenderDepth += 1
 	
-	text_file.close()
 
